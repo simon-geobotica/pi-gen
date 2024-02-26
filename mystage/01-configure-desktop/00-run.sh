1dbc5c676f95a35bb19c1fd2cee0aa5bac747cb4
simon-geobotica <133072301+simon-geobotica@users.noreply.github.com>
2024-02-26 12:20:23 +1000

    Updated desktop settings

---
+++ mystage/01-configure-desktop/00-run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -e
+
+install -m 644 files/defaults	"${ROOTFS_DIR}/usr/share/xfwm4/"
+
+install -m 644 files/desktop-bg.png "${ROOTFS_DIR}/usr/share/backgrounds/xfce/"
+
+xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitorHDMI-1/workspace0/last-image -s "${ROOTFS_DIR}/usr/share/backgrounds/xfce/desktop-bg.png"
