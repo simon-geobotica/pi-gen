6330fe562803866a839d16c27a1e369cc023fec3
simon-geobotica <133072301+simon-geobotica@users.noreply.github.com>
2024-02-27 10:30:58 +1000

    Added bluepy install

--- mystage/00-install-packages/01-run.sh
+++ mystage/00-install-packages/01-run.sh
@@ -1,3 +1,10 @@
 #!/bin/bash -e
 
 curl -fsSL https://tailscale.com/install.sh | sh
+
+git clone https://github.com/IanHarvey/bluepy.git
+cd bluepy
+python setup.py build
+python setup.py install
+cd ..
+rm -r bluepy
