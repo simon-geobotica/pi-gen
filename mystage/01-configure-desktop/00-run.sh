38c744d70af7aeceb38a4e6ebc8053f56e57e18b
simon-geobotica <133072301+simon-geobotica@users.noreply.github.com>
2024-02-27 13:23:08 +1000

    Add wi-fi

--- mystage/01-configure-desktop/00-run.sh
+++ mystage/01-configure-desktop/00-run.sh
@@ -5,3 +5,6 @@ install -m 644 files/defaults	"${ROOTFS_DIR}/usr/share/xfwm4/"
 install -m 644 files/desktop-bg.png "${ROOTFS_DIR}/usr/share/backgrounds/xfce/"
 
 #xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitorHDMI-1/workspace0/last-image -s "${ROOTFS_DIR}/usr/share/backgrounds/xfce/desktop-bg.png"
+
+# Add wi-fi config
+install -m 600 files/Telstra2034C9.nmconnection "${ROOTFS_DIR}/etc/NetworkManager/system-connections/"
