63f79c136647a3225979b66ce1106100cd6d2111
simon-geobotica <133072301+simon-geobotica@users.noreply.github.com>
2024-02-27 13:59:33 +1000

    Configure desktop background

--- mystage/01-configure-desktop/00-run.sh
+++ mystage/01-configure-desktop/00-run.sh
@@ -5,6 +5,8 @@ install -m 644 files/defaults	"${ROOTFS_DIR}/usr/share/xfwm4/"
 install -m 644 files/desktop-bg.png "${ROOTFS_DIR}/usr/share/backgrounds/xfce/"
 
 #xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitorHDMI-1/workspace0/last-image -s "${ROOTFS_DIR}/usr/share/backgrounds/xfce/desktop-bg.png"
+mkdir -p "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.config/xfce4/xfconf/xfce-perchannel-xml"
+install -m 644 files/xfce4-desktop.xml "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.config/xfce4/xfconf/xfce-perchannel-xml/"
 
 # Add wi-fi config
 install -m 600 files/Telstra2034C9.nmconnection "${ROOTFS_DIR}/etc/NetworkManager/system-connections/"
