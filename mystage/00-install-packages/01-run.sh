a86b6a12044f00d28f834d6567b3afae878a48e1
simon-geobotica <133072301+simon-geobotica@users.noreply.github.com>
2024-02-27 11:02:22 +1000

    Update 01-run.sh

--- mystage/00-install-packages/01-run.sh
+++ mystage/00-install-packages/01-run.sh
@@ -2,11 +2,6 @@
 
 curl -fsSL https://tailscale.com/install.sh | sh
 
-git clone https://github.com/IanHarvey/bluepy.git
-cd bluepy
-python setup.py build
-python setup.py install
-cd ..
-rm -r bluepy
+python -m pip install bluepy
 
 python -m pip install files/iis2iclx_wrapper-0.2.7-cp311-cp311-manylinux_2_17_aarch64.manylinux2014_aarch64.whl
