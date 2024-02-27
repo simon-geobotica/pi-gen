832226971089e10321797a524c183651233d4732
simon-geobotica <133072301+simon-geobotica@users.noreply.github.com>
2024-02-27 11:27:28 +1000

    Update 00-run.sh

--- mystage/02-copy-files/00-run.sh
+++ mystage/02-copy-files/00-run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
-mkdir "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/Desktop/"
-mkdir "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/Desktop/GeoLidar/"
+#mkdir "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/Desktop/"
+mkdir -p "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/Desktop/GeoLidar/"
 cp -r full-source/GeoLiDAR/RPI_code/	"${ROOTFS_DIR}/home/${FIRST_USER_NAME}/Desktop/GeoLidar/"
