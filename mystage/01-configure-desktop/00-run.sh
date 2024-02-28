224a936fd70e83b0e4813b18ca44b4f464809ab2
simon-geobotica <133072301+simon-geobotica@users.noreply.github.com>
2024-02-28 10:37:16 +1000

    change permissions on folders

--- mystage/01-configure-desktop/00-run.sh
+++ mystage/01-configure-desktop/00-run.sh
@@ -5,7 +5,7 @@ install -m 644 files/defaults	"${ROOTFS_DIR}/usr/share/xfwm4/"
 install -m 644 files/desktop-bg.png "${ROOTFS_DIR}/usr/share/backgrounds/xfce/"
 
 #xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitorHDMI-1/workspace0/last-image -s "${ROOTFS_DIR}/usr/share/backgrounds/xfce/desktop-bg.png"
-mkdir -p "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.config/xfce4/xfconf/xfce-perchannel-xml"
+mkdir -p -m a=rwx "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.config/xfce4/xfconf/xfce-perchannel-xml"
 install -m 644 files/xfce4-desktop.xml "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.config/xfce4/xfconf/xfce-perchannel-xml/"
 
 # Add wi-fi config
