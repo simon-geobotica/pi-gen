e5b693550a04dd62572fc86a3bbfb5a18437df13
simon-geobotica <133072301+simon-geobotica@users.noreply.github.com>
2024-03-08 12:13:08 +1000

    Fixed things which were installing on host system

--- mystage/00-install-packages/01-run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -e
-
-curl -fsSL https://tailscale.com/install.sh | sh
-
-#python -m pip install bluepy
-
-python -m pip install files/iis2iclx_wrapper-0.2.7-cp311-cp311-manylinux_2_17_aarch64.manylinux2014_aarch64.whl --break-system-packages
--- mystage/02-copy-files/00-run.sh
+++ mystage/02-copy-files/00-run.sh
@@ -3,3 +3,5 @@
 #mkdir "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/Desktop/"
 mkdir -p "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/Desktop/GeoLidar/"
 cp -r full-source/GeoLiDAR/RPI_code/	"${ROOTFS_DIR}/home/${FIRST_USER_NAME}/Desktop/GeoLidar/"
+
+install -m 644 files/iis2iclx_wrapper-0.2.7-cp311-cp311-manylinux_2_17_aarch64.manylinux2014_aarch64.whl "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/Desktop/"
